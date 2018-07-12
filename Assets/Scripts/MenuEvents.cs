df9bf7a1424469d3838222c6003c74e606e10b1e
DESKTOP-IQE7QK3\jonas <jonasrdsantos@gmail.com>
2018-07-11 21:53:06 -0300

    Created script to handle button events

---
+++ Assets/Scripts/MenuEvents.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuEvents : MonoBehaviour {
+    // Text events
+    public Text eventsLog;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TestStagesButton()
+    {
+        setEventsLogText("TestStagesButton()");
+    }
+
+    public void UITestButton()
+    {
+        setEventsLogText("UITestButton()");
+    }
+
+    public void OptionsButton()
+    {
+        setEventsLogText("OptionsButton()");
+    }
+
+    public void QuitButton()
+    {
+        setEventsLogText("QuitButton()");
+        Application.Quit();
+    }
+
+    // Change events log text
+    private void setEventsLogText(string text)
+    {
+        if(eventsLog == null)
+        {
+            return;
+        }
+        eventsLog.text = text;
+    }
+}
