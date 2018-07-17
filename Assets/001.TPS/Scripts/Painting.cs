d04a42e522d0978fdb399347ef60dcc08355da09
DESKTOP-IQE7QK3\jonas <jonasrdsantos@gmail.com>
2018-07-17 05:59:57 -0300

    Created a painting prefab

---
+++ Assets/001.TPS/Scripts/Painting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : MonoBehaviour {
+
+    public Texture imagem;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Material material = new Material(Shader.Find("Diffuse"));
+        material.mainTexture = imagem;
+        gameObject.GetComponent<Renderer>().material = material;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
